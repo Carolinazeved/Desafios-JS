70ddd8710830bd436df44fdf1c837348d872b371
Carolin Azevedo <carolinevolney@hotmail.com>
2024-12-30 14:50:45 -0300

    codigo Ticket

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,13 +1,12 @@
-function comprar() {
-    //recuperar as informaçõpes do formulario
+function comprar() {//recuperar as informaçõpes do formulario
  let tipo = document.getElementById('tipo-ingresso');
  let qtd = parseInt(document.getElementById('qtd').value);//parseint para retornar como numero inteiro
    
  if (tipo.value == 'pista') {//condicional para comprar pista
       comprarPista(qtd);
-  }else if (tipo.value == 'superior') {
+  }else if (tipo.value == 'superior') {//condicional para comprar superior
     comprarSuperior(qtd);
-  }else {
+  }else {//condicional para comprar inferior
     comprarInferior(qtd);
   }
 }
@@ -15,7 +14,7 @@ function comprar() {
 function comprarPista(qtd) {//função para comprar pista
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);//recuperar a quantidade 
     if(qtd > qtdPista){//condicionais
-        alert('Quantidade indisponivel');
+        alert('Quantidade indisponivel');//quando a quantidade é maior do que o disponivel
     } else {
         qtdPista = qtdPista - qtd;//diminuir do total que tem para o que é comprado
         document.getElementById('qtd-pista').textContent = qtdPista;
@@ -26,7 +25,7 @@ function comprarPista(qtd) {//função para comprar pista
 function comprarSuperior(qtd) {//função para comprar superior
     let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);//recuperar a quantidade 
     if(qtd > qtdSuperior){//condicionais
-        alert('Quantidade indisponivel');
+        alert('Quantidade indisponivel');//quando a quantidade é maior do que o disponivel
     } else {
         qtdSuperior = qtdSuperior - qtd;//diminuir do total que tem para o que é comprado
         document.getElementById('qtd-superior').textContent = qtdSuperior;
@@ -37,7 +36,7 @@ function comprarSuperior(qtd) {//função para comprar superior
 function comprarInferior(qtd) {//função para comprar Inferior
     let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);//recuperar a quantidade 
     if(qtd > qtdInferior){//condicionais
-        alert('Quantidade indisponivel');
+        alert('Quantidade indisponivel');//quando a quantidade é maior do que o disponivel
     } else {
         qtdInferior = qtdInferior - qtd;//diminuir do total que tem para o que é comprado
         document.getElementById('qtd-inferior').textContent = qtdInferior;
